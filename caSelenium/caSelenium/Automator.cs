772ccf73d09dc31f02856a3a0a6d19420f7ff0e5
Cristiano Pimenta <36493435+cristianopimenta@users.noreply.github.com>
2023-12-14 15:51:29 -0300

    Modelo Simples
    Modelo simples com selenium

---
+++ caSelenium/caSelenium/Automator.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caSelenium
+{
+    public class Automator
+    {
+        public IWebDriver driver;
+
+        public Automator()
+        {
+            driver = new ChromeDriver();
+
+                }
+
+        public string TesteWeb()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com.br");
+            driver.FindElement(By.Name("q")).SendKeys("hello");
+            driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[6]/center/input[1]")).Click();
+           var text = driver.FindElement(By.XPath("//*[@id=\"gsr\"]")).Text;
+            
+           return text;
+        }
+
+    }
+}
---
+++ caSelenium/caSelenium/Program.cs
@@ -0,0 +1,9 @@
+﻿using caSelenium;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+var web = new Automator();
+var text = web.TesteWeb();
+
+Console.WriteLine(text);
+Console.ReadLine();
